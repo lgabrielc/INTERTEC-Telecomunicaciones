1e2d78a41d8b9b447cf536542d7cced3b8069b9b
lgabrielc <kidmeg100@hotmail.com>
2021-09-21 16:46:33 -0500

    corregido torre2

--- resources/views/livewire/admin/servidor/show-servidor.blade.php
+++ resources/views/livewire/admin/servidor/show-servidor.blade.php
@@ -148,7 +148,7 @@
                                                     </td>
                                                     <td class="px-5 py-1 border-b border-gray-200 bg-white text-base">
                                                         <p class="text-gray-900 whitespace-no-wrap">
-                                                            {{ $servidor->estado_id }}
+                                                            {{ $servidor->estado->nombre }}
                                                         </p>
                                                     </td>
                                                     <td
